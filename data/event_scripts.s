0f46d3aa712eb3fc7a5eb07509850e9dd4596348
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2024-10-11 01:38:12 +0200

    Isabell fix, add new mapgroups

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1075,3 +1075,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Whitecliff_Castle_Infirmary/scripts.inc"
